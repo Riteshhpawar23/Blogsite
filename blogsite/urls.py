26b6a18b7e51663ff89f513013091c2dbc24007b
Ritesh Git <riteshpawar0223@gmail.com>
2025-09-05 19:42:22 +0530

    home element added

--- blogsite/urls.py
+++ blogsite/urls.py
@@ -21,4 +21,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('categories/', views.categories, name='categories'),  
 ]
--- blogsite/views.py
+++ blogsite/views.py
@@ -3,4 +3,12 @@ from django.shortcuts import render
 
 def home(request):
     """Render the home page"""
+    
     return render(request, 'home.html')
+
+def about(request):
+    """Render the about page"""
+    return render(request, 'about.html')
+def categories(request):
+    """Render the Categories page"""
+    return render(request, 'categories.html')
